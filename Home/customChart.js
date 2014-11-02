814b2589ea02e81cf942558363e7a5dfc72e89fb
kech17 <kevinchang@live.ca>
2014-11-02 17:25:56 -0500

    who i am body text and few adjustments and button hover

--- Home/customChart.js
+++ Home/customChart.js
@@ -1,14 +1,14 @@
 var hardwareChart = $("#hardwareChart").get(0).getContext("2d");
 var hardwareData = {
-    labels: ["Prototyping", "Robotics", "IC Analysis", "Processors", "Automation"],
+    labels: ["Web Dev", "IC Analysis", "Mobile", "Robotics", "OOP", "Prototyping", "Low-level" ],
     datasets: [
         {
-            label: "Hardware Experience",
+            label: "Technical Experience",
             fillColor: "rgba(119,221,119,0.5)",
             strokeColor: "rgba(119,221,119,0.8)",
             highlightFill: "rgba(119,221,119,0.75)",
             highlightStroke: "rgba(119,221,119,1)",
-            data: [80, 70, 90, 60, 70]
+            data: [70, 50, 80, 60, 90, 70, 40]
         }
     ]
 };
